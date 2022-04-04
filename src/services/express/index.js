aa89cd917ad1d585f2903d6108c48785802b897a
ketodd2018 <57691657+ketodd2018@users.noreply.github.com>
2022-04-04 13:52:12 -0500

    Following along with the course. Finished Structured Logging

--- src/services/express/index.js
+++ src/services/express/index.js
@@ -2,13 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import compression from 'compression'
 import morgan from 'morgan'
+import json from 'morgan-json'
+import path from 'path'
+import rfs from 'rotating-file-stream'
+import generateSafeId from 'generate-safe-id'
 import bodyParser from 'body-parser'
 import { errorHandler as queryErrorHandler } from 'querymen'
 import { errorHandler as bodyErrorHandler } from 'bodymen'
 import { env } from '../../config'
 
 export default (apiRoot, routes) => {
-  const app = express()
+  const app = express();
+  const logDirectory = path.join(__dirname, 'logs');
+  const accessLogStream = rfs('access.log', {
+    interval: '5d',
+    path: logDirectory
+  });
 
   /* istanbul ignore next */
   if (env === 'production' || env === 'development') {
@@ -16,7 +25,18 @@ export default (apiRoot, routes) => {
     app.use(compression())
   }
 
-  app.use(morgan('tiny'))
+  morgan.token('id', function getId() {
+    return generateSafeId();
+  });
+
+  const format = json({
+    short: ':id :method :url :status',
+    length: ':res[content-length]',
+    responseTime: ':response-time ms'
+  });
+
+  app.use(morgan(format, {stream: accessLogStream}));
+  app.use(morgan(format));
 
 
   app.use(bodyParser.urlencoded({ extended: false }))
